766f790b367dd6a16f5d17aa3fbbcf731a3ac746
Ying Huang <yhuang@imvu.com>
2018-04-19 21:09:42 -0700

    n

--- fastai/models/nasnet.py
+++ fastai/models/nasnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.model_zoo as model_zoo
+from torch.autograd import Variable
 
 pretrained_settings = {
     'nasnetalarge': {
@@ -376,7 +377,7 @@ class NormalCell(nn.Module):
 class ReductionCell0(nn.Module):
 
     def __init__(self, in_channels_left, out_channels_left, in_channels_right, out_channels_right):
-        super(ReductionCell0, self).__init__()
+        super(ReductionCell0, self).__init__() 
         self.conv_prev_1x1 = nn.Sequential()
         self.conv_prev_1x1.add_module('relu', nn.ReLU())
         self.conv_prev_1x1.add_module('conv', nn.Conv2d(in_channels_left, out_channels_left, 1, stride=1, bias=False))
@@ -487,7 +488,7 @@ class NASNetALarge(nn.Module):
 
     def __init__(self, use_classifer=False, num_classes=1001):
         super(NASNetALarge, self).__init__()
-        self.use_classifer, self.num_classes = use_classifer, num_classes
+        self.use_classifer,self.num_classes = use_classifer,num_classes
 
         self.conv0 = nn.Sequential()
         self.conv0.add_module('conv', nn.Conv2d(in_channels=3, out_channels=96, kernel_size=3, padding=0, stride=2,
