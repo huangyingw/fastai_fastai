2bc0cfc527723d0d70d3c075830d6ba544c55990
Ying Huang <yhuang@imvu.com>
2018-05-12 10:54:24 -0700

    n

--- courses/dl1/save_restore.py
+++ courses/dl1/save_restore.py
@@ -1,13 +1,10 @@
-from PIL import Image
 from fastai.conv_learner import ConvLearner
 from fastai.dataset import ImageClassifierData
-from fastai.transforms import tfms_from_model
+from fastai.transforms import tfms_from_model, transforms_side_on
 from torchvision.models import resnet34
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 import os.path
-import pprint
 import subprocess
 import torch
 
@@ -36,58 +33,13 @@ arch = resnet34
 data = ImageClassifierData.from_paths(PATH, tfms=tfms_from_model(arch, sz))
 
 
-def plots(ims, figsize=(12, 6), rows=1, titles=None):
-    f = plt.figure(figsize=figsize)
-    for i in range(len(ims)):
-        sp = f.add_subplot(rows, len(ims) // rows, i + 1)
-        sp.axis('Off')
-        if titles is not None:
-            sp.set_title(titles[i], fontsize=16)
-            plt.imshow(ims[i])
-    plt.show()
-
-
-def plot_val_with_title(idxs, title, probs):
-    imgs = np.stack([data.val_ds[x][0] for x in idxs])
-    title_probs = [probs[x] for x in idxs]
-    print(title)
-    return plots(data.val_ds.denorm(imgs), rows=1, titles=title_probs)
-
-
-def most_by_mask(mask, mult, probs):
-    idxs = np.where(mask)[0]
-    return idxs[np.argsort(mult * probs[idxs])[:4]]
-
-
-def most_by_correct(y, is_correct, preds, probs):
-    mult = -1 if (y == 1) == is_correct else 1
-    return most_by_mask(((preds == data.val_y) == is_correct)
-                        & (data.val_y == y), mult, probs)
-
+tfms = tfms_from_model(
+    resnet34, sz, aug_tfms=transforms_side_on, max_zoom=1.1)
 
 def learn1():
-
+    data = ImageClassifierData.from_paths(PATH, tfms=tfms)
     learn = ConvLearner.pretrained(arch, data, precompute=True)
-    learn.lr_find()
+    learn.fit(1e-2, 1, saved_model_name='lesson1_1e-2')
     learn.sched.plot_lr()
-    learn.sched.plot()
-
-    pp = pprint.PrettyPrinter(indent=4)
-    print('learn --> ')
-    pp.pprint(learn)
-
-    print('data.val_y --> ', data.val_y)
-
-    print('data.classes --> ', data.classes)
-
-    log_preds = learn.predict()
-    print('log_preds.shape --> ', log_preds.shape)
-    log_preds.shape
-
-    print('log_preds[:10] --> ', log_preds[:10])
-
-    preds = np.argmax(log_preds, axis=1)  # from log probabilities to 0 or 1
-    probs = np.exp(log_preds[:, 1])        # pr(dog)
-
 
 learn1()
