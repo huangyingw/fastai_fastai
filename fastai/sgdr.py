7480328dee0d230fcb110fd9f2b403991618c6ce
Ying Huang <yhuang@imvu.com>
2018-05-12 14:32:10 -0700

    n

--- fastai/sgdr.py
+++ fastai/sgdr.py
@@ -138,6 +138,22 @@ class LossRecorder(Callback):
         if not in_ipynb():
             plt.savefig(os.path.join(self.save_path, 'lr_plot.png'))
 
+    def state_dict(self):
+        """Returns the state of the scheduler as a :class:`dict`.
+
+        It contains an entry for every variable in self.__dict__ which
+        is not the optimizer.
+        """
+        return {key: value for key, value in self.__dict__.items()}
+
+    def load_state_dict(self, state_dict):
+        """Loads the schedulers state.
+
+        Arguments:
+            state_dict (dict): scheduler state. Should be an object returned
+                from a call to :meth:`state_dict`.
+        """
+        self.__dict__.update(state_dict)
 
 class LR_Updater(LossRecorder):
     '''
