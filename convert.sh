978ead655f011d83032a17e2a720a156f059b6c5
Ying Huang <yhuang@imvu.com>
2018-12-21 20:50:38 -0800

    n

--- convert.sh
+++ convert.sh
@@ -6,5 +6,8 @@ cd "$SCRIPTPATH"
 find . -type f -name \*.ipynb -exec jupyter nbconvert --to=python --template=python.tpl {} \;
 find . -type f -name *.ipynb | while read ss
 do
-    autopep8 --in-place "${ss%.ipynb}.py"
+    pyFile="${ss%.ipynb}.py"
+    sed -i"" '/^$/d' "$pyFile"
+    sed -i"" '/^#$/d' "$pyFile"
+    autopep8 --in-place "$pyFile"
 done
