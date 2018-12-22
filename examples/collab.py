c36307b85d91996051d5116e899a7512953dd7d2
Ying Huang <yhuang@imvu.com>
2018-12-21 20:48:54 -0800

    n

--- examples/collab.py
+++ examples/collab.py
@@ -1,30 +1,14 @@
-
 # coding: utf-8
-
-from fastai import *          # Quick access to most common functionality
 from fastai.collab import *   # Quick access to collab filtering functionality
-
-
 # ## Collaborative filtering example
-
 # `collab` models use data in a `DataFrame` of user, items, and ratings.
-
 path = untar_data(URLs.ML_SAMPLE)
 path
-
-
 ratings = pd.read_csv(path / 'ratings.csv')
 series2cat(ratings, 'userId', 'movieId')
 ratings.head()
-
-
 data = CollabDataBunch.from_df(ratings, seed=42)
-
-
 y_range = [0, 5.5]
-
-
 # That's all we need to create and train a model:
-
 learn = collab_learner(data, n_factors=50, y_range=y_range)
 learn.fit_one_cycle(4, 5e-3)
--- examples/dogs_cats.py
+++ examples/dogs_cats.py
@@ -1,45 +1,21 @@
-
 # coding: utf-8
-
 get_ipython().run_line_magic('reload_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
-
-from fastai import *
 from fastai.vision import *
-
-
 # # Dogs and cats
-
 # ## Resnet 34
-
 path = untar_data(URLs.DOGS)
 path
-
-
 data = ImageDataBunch.from_folder(path, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
-img, label = data.valid_ds[-1]
-img.show(title=data.classes[label])
-
-
+data.show_batch(rows=4)
 learn = create_cnn(data, models.resnet34, metrics=accuracy)
 learn.fit_one_cycle(1)
-
-
 learn.unfreeze()
 learn.fit_one_cycle(6, slice(1e-5, 3e-4), pct_start=0.05)
-
-
 accuracy(*learn.TTA())
-
-
 # ## rn50
-
 learn = create_cnn(data, models.resnet50, metrics=accuracy)
-learn.fit_one_cycle(6)
-
-
+learn.fit_one_cycle(1)
 learn.unfreeze()
 learn.fit_one_cycle(6, slice(1e-5, 3e-4), pct_start=0.05)
-
-
 accuracy(*learn.TTA())
