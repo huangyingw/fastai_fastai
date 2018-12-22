8aea96cc767bf4c3f231e27290a71af0f7f38442
Ying Huang <yhuang@imvu.com>
2018-12-21 21:14:47 -0800

    n

--- courses/ml1/lesson1-rf.py
+++ courses/ml1/lesson1-rf.py
@@ -81,12 +81,12 @@
 # ## Imports
 from fastai.imports import *
 from fastai.structured import *
-from pandas_summary import DataFrameSummary
-from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from IPython.display import display
 from sklearn import metrics
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 PATH = "data/bulldozers/"
-get_ipython().system('ls {PATH}')
+# get_ipython().system('ls {PATH}')
 # # Introduction to *Blue Book for Bulldozers*
 # ## About...
 # ### ...our teaching
@@ -135,9 +135,13 @@ get_ipython().system('ls {PATH}')
 df_raw = pd.read_csv(f'{PATH}Train.csv', low_memory=False,
                      parse_dates=["saledate"])
 # In any sort of data science work, it's **important to look at your data**, to make sure you understand the format, how it's stored, what type of values it holds, etc. Even if you've read descriptions about your data, the actual data may not be what you expect.
+
+
 def display_all(df):
     with pd.option_context("display.max_rows", 1000, "display.max_columns", 1000):
         display(df)
+
+
 display_all(df_raw.tail().T)
 display_all(df_raw.describe(include='all').T)
 # It's important to note what metric is being used for a project. Generally, selecting the metric(s) is an important part of the project setup. However, in this case Kaggle tells us what metric to use: RMSLE (root mean squared log error) between the actual and predicted auction prices. Therefore we take the log of the prices, so that RMSE will give us what we need.
@@ -185,7 +189,11 @@ m.score(df, y)
 # The error for the pictured data points is lowest for the model on the far right (the blue curve passes through the red points almost perfectly), yet it's not the best choice.  Why is that?  If you were to gather some new data points, they most likely would not be on that curve in the graph on the right, but would be closer to the curve in the middle graph.
 #
 # This illustrates how using all our data can lead to **overfitting**. A validation set helps diagnose this problem.
+
+
 def split_vals(a, n): return a[:n].copy(), a[n:].copy()
+
+
 n_valid = 12000  # same as Kaggle's test set size
 n_trn = len(df) - n_valid
 raw_train, raw_valid = split_vals(df_raw, n_trn)
@@ -195,14 +203,21 @@ X_train.shape, y_train.shape, X_valid.shape
 # # Random Forests
 # ## Base model
 # Let's try our model again, this time with separate training and validation sets.
+
+
 def rmse(x, y): return math.sqrt(((x - y)**2).mean())
+
+
 def print_score(m):
     res = [rmse(m.predict(X_train), y_train), rmse(m.predict(X_valid), y_valid),
-                m.score(X_train, y_train), m.score(X_valid, y_valid)]
-    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
+           m.score(X_train, y_train), m.score(X_valid, y_valid)]
+    if hasattr(m, 'oob_score_'):
+        res.append(m.oob_score_)
     print(res)
+
+
 m = RandomForestRegressor(n_jobs=-1)
-get_ipython().run_line_magic('time', 'm.fit(X_train, y_train)')
+m.fit(X_train, y_train)
 print_score(m)
 # An r^2 in the high-80's isn't bad at all (and the RMSLE puts us around rank 100 of 470 on the Kaggle leaderboard), but we can see from the validation set score that we're over-fitting badly. To understand this issue, let's simplify things down to a single small tree.
 # ## Speeding things up
@@ -210,7 +225,7 @@ df_trn, y_trn, nas = proc_df(df_raw, 'SalePrice', subset=30000, na_dict=nas)
 X_train, _ = split_vals(df_trn, 20000)
 y_train, _ = split_vals(y_trn, 20000)
 m = RandomForestRegressor(n_jobs=-1)
-get_ipython().run_line_magic('time', 'm.fit(X_train, y_train)')
+m.fit(X_train, y_train)
 print_score(m)
 # ## Single tree
 m = RandomForestRegressor(n_estimators=1, max_depth=3, bootstrap=False, n_jobs=-1)
@@ -232,7 +247,7 @@ print_score(m)
 preds = np.stack([t.predict(X_valid) for t in m.estimators_])
 preds[:, 0], np.mean(preds[:, 0]), y_valid[0]
 preds.shape
-plt.plot([metrics.r2_score(y_valid, np.mean(preds[:i + 1], axis=0)) for i in range(10)]);
+plt.plot([metrics.r2_score(y_valid, np.mean(preds[:i + 1], axis=0)) for i in range(10)])
 # The shape of this curve suggests that adding more trees isn't going to help us much. Let's check. (Compare this to our original model on a sample)
 m = RandomForestRegressor(n_estimators=20, n_jobs=-1)
 m.fit(X_train, y_train)
@@ -264,7 +279,7 @@ y_train, y_valid = split_vals(y_trn, n_trn)
 # The basic idea is this: rather than limit the total amount of data that our model can access, let's instead limit it to a *different* random subset per tree. That way, given enough trees, the model can still see *all* the data, but for each individual tree it'll be just as fast as if we had cut down our dataset as before.
 set_rf_samples(20000)
 m = RandomForestRegressor(n_jobs=-1, oob_score=True)
-get_ipython().run_line_magic('time', 'm.fit(X_train, y_train)')
+m.fit(X_train, y_train)
 print_score(m)
 # Since each additional tree allows the model to see more data, this approach can make additional trees more useful.
 m = RandomForestRegressor(n_estimators=40, n_jobs=-1, oob_score=True)
@@ -274,6 +289,8 @@ print_score(m)
 # We revert to using a full bootstrap sample in order to show the impact of other over-fitting avoidance methods.
 reset_rf_samples()
 # Let's get a baseline for this full set to compare to.
+
+
 def dectree_max_depth(tree):
     children_left = tree.children_left
     children_right = tree.children_right
@@ -283,10 +300,12 @@ def dectree_max_depth(tree):
             left_max = 1 + walk(children_left[node_id])
             right_max = 1 + walk(children_right[node_id])
             return max(left_max, right_max)
-        else: # leaf
+        else:  # leaf
             return 1
     root_node_id = 0
     return walk(root_node_id)
+
+
 m = RandomForestRegressor(n_estimators=40, n_jobs=-1, oob_score=True)
 m.fit(X_train, y_train)
 print_score(m)
